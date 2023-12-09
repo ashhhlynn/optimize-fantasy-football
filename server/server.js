4b811241cebc0b355e54795d86e8dce760f4ba1f
ashhhlynn <ashleylynnwelch@gmail.com>
2023-12-09 00:16:36 -0600

    updated contest data

--- server/server.js
+++ server/server.js
@@ -131,7 +131,7 @@ app.get("/trcaptainplayers", async (req, res) => {
 })
 
 app.get("/moncaptainplayers", async (req, res) => { 
-    let num = 96505
+    let num = 96908
     const queue = await updateCaptain(num)
     const crownsQueue = queue.crownsQueue
     const flexesQueue = queue.flexesQueue
@@ -290,7 +290,7 @@ async function updateClassic(num) {
 }
 
 app.get("/classicplayers", async (req, res) => { 
-    let num = 96481
+    let num = 96888
     const uniques = await updateClassic(num)
     let q = uniques.filter(d => d.Position === "QB")
     let r = uniques.filter(d => d.Position === "RB")
