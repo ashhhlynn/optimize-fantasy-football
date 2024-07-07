4050856c7aa25bedabd0c34e3ba8e3cd5cd8ec2d
ashhhlynn <ashleylynnwelch@gmail.com>
2024-07-07 18:12:03 -0500

    Minor syntax cleaning

--- server/server.js
+++ server/server.js
@@ -19,7 +19,7 @@ function fetchSleeperProjections() {
     .then(data => {
         data.map((p) => {
             if (p.stats.pts_ppr > 0) {  
-                var name = p.player.position === "DEF" ?  p.player.last_name : p.player.first_name + ' ' + p.player.last_name 
+                let name = p.player.position === "DEF" ?  p.player.last_name : p.player.first_name + ' ' + p.player.last_name
                 sleeperProjections[name] = p.stats.pts_ppr
             }
         });
@@ -34,15 +34,15 @@ function fetchClassicPlayers() {
     .then(data => {
         for (let z=0; z < data.draftables.length; z++) {
             if (data.draftables[z].draftStatAttributes[0].id === 90) {
-                let p = data.draftables[z]
-                let sleeper = Object.keys(sleeperProjections).find(key => key === p.displayName || key.slice(0,10) === p.displayName.slice(0,10))
+                let p = data.draftables[z];
+                let sleeper = Object.keys(sleeperProjections).find(key => key === p.displayName || key.slice(0,10) === p.displayName.slice(0,10));
                 var proj = sleeper !== undefined ? sleeperProjections[sleeper] : 0
-                var details = {
+                let details = {
                     ...p, 
                     Projection: proj, 
                     [p.position]: 1, 
                     [z]: 1
-                }    
+                };    
                 classicPlayers.push(details);
                 if (proj > 0) {
                     classicCombined[z] = details
@@ -79,10 +79,10 @@ function fetchCaptainPlayers() {
 };
 
 function getCaptainPlayers(data) {
-    let [flexesQueue, crownsQueue] = [[],[]]
+    let [flexesQueue, crownsQueue] = [[],[]];
     data.map((p) => {
         if (p.draftStatAttributes[0].id === 90) {
-            let sleeper = Object.keys(sleeperProjections).find(key => key === p.displayName || key.slice(0,10) === p.displayName.slice(0,10))
+            let sleeper = Object.keys(sleeperProjections).find(key => key === p.displayName || key.slice(0,10) === p.displayName.slice(0,10));
             var proj = sleeper !== undefined ? sleeperProjections[sleeper] : 0
             let details = {
                 Name: p.displayName,
@@ -94,34 +94,34 @@ function getCaptainPlayers(data) {
                 DraftTableId: p.playerId,
                 Status: p.status,
                 Salary: p.salary,
-            }
-            var ind = crownsQueue.findIndex(x => x.DraftTableId === p.playerId )
+            };
+            let ind = crownsQueue.findIndex(x => x.DraftTableId === p.playerId );
             if (ind === -1) {
                 let newPlayer = {
                     ...details, 
                     CROWN: 1, 
                     [crownsQueue.length]: 1, 
                     Projection: proj * 1.5
-                }
-                crownsQueue.push(newPlayer)
+                };
+                crownsQueue.push(newPlayer);
             } else {
                 let newPlayer = {
                     ...details, 
                     FLEX: 1, 
                     [ind]: 1, 
                     Projection: proj
-                }
-                flexesQueue.push(newPlayer)
+                };
+                flexesQueue.push(newPlayer);
             }
         }
     });
-    let combinedQueue = [...crownsQueue, ...flexesQueue]
+    let combinedQueue = [...crownsQueue, ...flexesQueue];
     return { flexesQueue, combinedQueue }    
 }; 
 
 app.get("/dates", (req, res) => { 
-    let d1 = new Date(captainQueue1.flexesQueue[0].Time.substr(0,10))
-    let d2 = new Date(captainQueue2.flexesQueue[0].Time.substr(0,10))
+    let d1 = new Date(captainQueue1.flexesQueue[0].Time.substr(0,10));
+    let d2 = new Date(captainQueue2.flexesQueue[0].Time.substr(0,10));
     const dayNames = ["Mon", "Tues", "Wed", "Thurs", "Fri", "Sat", "Sun"];
     res.json({
         clDate: classicPlayers[0].competition.startTime.substr(5,5), 
@@ -133,9 +133,9 @@ app.get("/dates", (req, res) => {
 });
 
 app.get("/classicplayers", (req, res) => { 
-    let sortedPlayers = { qall: classicPlayers, qqb: [], qrb: [], qwr: [], qte: [], qdst: [], qflex: [] }
+    let sortedPlayers = { qall: classicPlayers, qqb: [], qrb: [], qwr: [], qte: [], qdst: [], qflex: [] };
     for (let i=0; i < classicPlayers.length; i++) {
-        let pos =  'q' + classicPlayers[i].position.toLowerCase()
+        let pos =  'q' + classicPlayers[i].position.toLowerCase();
         if (pos === 'qrb' || pos === 'qwr' || pos === 'qte') { sortedPlayers['qflex'] = [...sortedPlayers['qflex'], classicPlayers[i]] }
         sortedPlayers[pos] = [...sortedPlayers[pos],  classicPlayers[i]] 
     } 
@@ -143,23 +143,23 @@ app.get("/classicplayers", (req, res) => {
 });
 
 app.get("/classicoptimizer", (req, res) => { 
-    let sortedResults = { lineup: [], qb: [], rb: [], wr: [], te: [], dst: [], flex: [], result: 0, remSal: 50000 }
-    let classicConstraintNew = getClassicConstraint()
-    let results = optimizeClassic(classicConstraintNew, classicCombined, .025)
-    let endResult = sortClassicResults(results, sortedResults)
+    let sortedResults = { lineup: [], qb: [], rb: [], wr: [], te: [], dst: [], flex: [], result: 0, remSal: 50000 };
+    let classicConstraintNew = getClassicConstraint();
+    let results = optimizeClassic(classicConstraintNew, classicCombined, .025);
+    let endResult = sortClassicResults(results, sortedResults);
     res.json(endResult);
 });
 
 app.post("/classicoptimize", (req, res) => {
-    let lineupPlayers = req.body.lp
-    let fl = req.body.fl 
-    let sortedResults = { lineup: [...lineupPlayers, ...fl], qb: [], rb: [], wr: [], te: [], dst: [], flex: fl, result: 0, remSal: 50000 }
-    let classicConstraintNew = getClassicConstraint()
-    let classicAll = {...classicCombined}    
+    let lineupPlayers = req.body.lp;
+    let fl = req.body.fl; 
+    let sortedResults = { lineup: [...lineupPlayers, ...fl], qb: [], rb: [], wr: [], te: [], dst: [], flex: fl, result: 0, remSal: 50000 };
+    let classicConstraintNew = getClassicConstraint();
+    let classicAll = {...classicCombined};
     for (let i = 0; i < lineupPlayers.length; i++) {
-        delete classicAll[Object.keys(lineupPlayers[i])[0]]
-        delete classicAll[Number(Object.keys(lineupPlayers[i])[0]) + 1000]
-        let playerPos = lineupPlayers[i].position
+        delete classicAll[Object.keys(lineupPlayers[i])[0]];
+        delete classicAll[Number(Object.keys(lineupPlayers[i])[0]) + 1000];
+        let playerPos = lineupPlayers[i].position;
         classicConstraintNew[`${playerPos}`]['min'] -= 1 
         classicConstraintNew[`${playerPos}`]['max'] -= 1 
         sortedResults['remSal'] -= lineupPlayers[i].salary   
@@ -167,15 +167,15 @@ app.post("/classicoptimize", (req, res) => {
         sortedResults[playerPos.toLowerCase()] = [...sortedResults[playerPos.toLowerCase()], lineupPlayers[i]]
     }  
     if (fl.length !== 0) {        
-        delete classicAll[Object.keys(fl[0])[0]]
-        delete classicAll[Number(Object.keys(fl[0])[0]) + 1000]
+        delete classicAll[Object.keys(fl[0])[0]];
+        delete classicAll[Number(Object.keys(fl[0])[0]) + 1000];
         classicConstraintNew['FLEX'] = { 'min': 0, 'max': 0 }
         sortedResults['remSal'] -= fl[0].salary   
         sortedResults['result'] += fl[0].Projection    
     }
     classicConstraintNew['salary'] = { 'max': sortedResults['remSal'] }    
-    let results = optimizeClassic(classicConstraintNew, classicAll, 0)
-    let endResult = sortClassicResults(results, sortedResults)
+    let results = optimizeClassic(classicConstraintNew, classicAll, 0);
+    let endResult = sortClassicResults(results, sortedResults);
     res.json(endResult);
 });
 
@@ -199,7 +199,7 @@ function optimizeClassic(classicConstraintNew, classicAll, num) {
         constraints: classicConstraintNew,   
         variables: classicAll,
         options: { "tolerance": num }
-    }    
+    };    
     return solver.Solve(model);
 };
 
@@ -207,10 +207,10 @@ function sortClassicResults(results, sortedResults) {
     for (const [key, value] of Object.entries(results)) {
         if (value === 1) {
             if (classicCombined[key].FLEX === 1) {    
-                var player = classicCombined[key-1000]
+                var player = classicCombined[key-1000];
                 sortedResults['flex'] = [player]
             } else {
-                var player = classicCombined[key]
+                var player = classicCombined[key];
                 sortedResults[player.position.toLowerCase()] = [...sortedResults[player.position.toLowerCase()], player]
             }
             sortedResults['lineup'] = [...sortedResults['lineup'], player]
@@ -230,24 +230,24 @@ app.get("/captainplayers2", (req, res) => {
 }); 
 
 app.post("/optimizedcaptain1", (req, res) => {    
-    let results = optimizeCaptain(captainQueue1, req.body.fp, req.body.cp)
+    let results = optimizeCaptain(captainQueue1, req.body.fp, req.body.cp);
     res.json(results);
 });
 
 app.post("/optimizedcaptain2", (req, res) => {
-    let results = optimizeCaptain(captainQueue2, req.body.fp, req.body.cp)
+    let results = optimizeCaptain(captainQueue2, req.body.fp, req.body.cp);
     res.json(results)
 });
 
 function optimizeCaptain(queue, selectedLineup, selectedCrown) {
-    let combinedQueue = [...queue.combinedQueue]
-    let projTotal = 0
+    let combinedQueue = [...queue.combinedQueue];
+    let projTotal = 0;
     let captainConstraint = {
         'Salary': {'max': 50000},
         'CROWN': { 'min': 1, 'max': 1 },
         'FLEX': { 'min': 5, 'max': 5 }
-    }
-    let captainInt = { 'FLEX': 1, 'CROWN': 1 }
+    };
+    let captainInt = { 'FLEX': 1, 'CROWN': 1 };
     for (let i=0; i < queue.flexesQueue.length; i++) {
         captainConstraint[i] = {'max': 1}
         captainInt[i] = 1      
@@ -272,22 +272,22 @@ function optimizeCaptain(queue, selectedLineup, selectedCrown) {
         ints: captainInt,
         constraints: captainConstraint,
         variables: combinedQueue,
-    }
+    };
     const results = solver.Solve(model);
-    let salarySum = captainConstraint['Salary']['max']
+    let salarySum = captainConstraint['Salary']['max'];
     for (const [key, value] of Object.entries(results)) {
         if (value === 1) {
             if (combinedQueue[key].CROWN === 1) {
-                let crown = queue.flexesQueue.find(p => p.Name === combinedQueue[key].Name)
-                selectedCrown.push(crown)
+                let crown = queue.flexesQueue.find(p => p.Name === combinedQueue[key].Name);
+                selectedCrown.push(crown);
                 salarySum -= crown.Salary * 1.5
             } else {
-                selectedLineup.push(combinedQueue[key])
+                selectedLineup.push(combinedQueue[key]);
                 salarySum -= combinedQueue[key].Salary
             }
         }
     }
-    let projSum = results.result + projTotal
+    let projSum = results.result + projTotal;
     return { crown: selectedCrown, fps: selectedLineup, sSum: salarySum, pSum: projSum }
 };
 
